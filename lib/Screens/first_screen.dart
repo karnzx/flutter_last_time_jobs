2afc67a4d3fa8114d18d92998127646179c9d5ee
karnzx <karnzx-18762@hotmail.com>
2021-08-13 22:36:12 +0700

    add timeStamp listView

--- lib/Screens/first_screen.dart
+++ lib/Screens/first_screen.dart
@@ -47,196 +47,210 @@ class _FirstScreenState extends State<FirstScreen> {
           ),
         ));
   }
-}
 
-Widget lasTimeList(List<LastTime> lastTimes) {
-  if (lastTimes.isEmpty) {
-    return Text(
-      'No History yet',
-      style: TextStyle(
-          fontWeight: FontWeight.bold, fontSize: 30, color: Colors.red),
-    );
-  } else {
-    return Column(
-      children: <Widget>[
-        SizedBox(height: 10),
-        Text('History count : ${lastTimes.length} items',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                color: Colors.lightBlue)),
-        SizedBox(height: 30),
-        Expanded(
-          child: ListView.builder(
-            padding: const EdgeInsets.all(10),
-            itemCount: lastTimes.length,
-            itemBuilder: (context, index) {
-              final lastTime = lastTimes[index];
-              return Container(
-                margin: const EdgeInsets.only(bottom: 10),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    gradient: LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      colors: [
-                        Color(0xFF0D47A1),
-                        Color(0xFF1976D2),
-                        Color(0xFF42A5F5),
-                      ],
+  Widget lasTimeList(List<LastTime> lastTimes) {
+    if (lastTimes.isEmpty) {
+      return Text(
+        'No History yet',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 30, color: Colors.red),
+      );
+    } else {
+      return Column(
+        children: <Widget>[
+          SizedBox(height: 10),
+          Text('History count : ${lastTimes.length} items',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.lightBlue)),
+          SizedBox(height: 30),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(10),
+              itemCount: lastTimes.length,
+              itemBuilder: (context, index) {
+                final lastTime = lastTimes[index];
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 5,
+                        )
+                      ]),
+                  child: ListTile(
+                    leading: Container(
+                      width: 48,
+                      height: 48,
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      alignment: Alignment.center,
+                      child: const Icon(Icons.done, color: Colors.white),
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 5,
-                      )
-                    ]),
-                child: ListTile(
-                  leading: Container(
-                    width: 48,
-                    height: 48,
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    alignment: Alignment.center,
-                    child: const Icon(Icons.done, color: Colors.white),
-                  ),
-                  title: Text(
-                    lastTime.job,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                      color: Colors.white,
+                    title: Text(
+                      lastTime.job,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  trailing: IconButton(
-                    onPressed: () => showDialog(
+                    trailing: IconButton(
+                      onPressed: () => showDialog(
+                        context: context,
+                        builder: (context) => deleteDialog(context, lastTime),
+                      ),
+                      icon: Icon(
+                        Icons.delete_forever,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: RichText(
+                      text: TextSpan(
+                        style: DefaultTextStyle.of(context).style,
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: '${lastTime.category}',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white70,
+                                  fontSize: 17)),
+                          TextSpan(
+                              text: ' -- ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white70)),
+                          TextSpan(
+                              text:
+                                  '${DateFormat.yMd().format(lastTime.timeStamp.last)}',
+                              style: TextStyle(
+                                  color: Colors.white70, fontSize: 17)),
+                        ],
+                      ),
+                    ),
+                    onTap: () => showDialog(
                       context: context,
-                      builder: (context) => deleteDialog(context, lastTime),
-                    ),
-                    icon: Icon(
-                      Icons.delete_forever,
-                      color: Colors.white,
+                      builder: (context) => detailDialog(context, lastTime),
                     ),
                   ),
-                  subtitle: RichText(
-                    text: TextSpan(
-                      style: DefaultTextStyle.of(context).style,
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: '${lastTime.category}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white70,
-                                fontSize: 17)),
-                        TextSpan(
-                            text: ' -- ',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white70)),
-                        TextSpan(
-                            text:
-                                '${DateFormat.yMd().format(lastTime.timeStamp.last)}',
-                            style:
-                                TextStyle(color: Colors.white70, fontSize: 17)),
-                      ],
-                    ),
-                  ),
-                  onTap: () => showDialog(
-                    context: context,
-                    builder: (context) => detailDialog(context, lastTime),
-                  ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
+        ],
+      );
+    }
+  }
+
+  Widget detailDialog(BuildContext context, LastTime lastTime) {
+    var timeStamp = lastTime.timeStamp.reversed.toList();
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      title: Text(lastTime.job,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+      content: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                style: DefaultTextStyle.of(context).style,
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Category : ',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                  TextSpan(
+                      text: '${lastTime.category}',
+                      style: TextStyle(fontSize: 20)),
+                ],
+              ),
+            ),
+            SizedBox(width: 20),
+            Container(
+              height: 100.0,
+              width: 200.0,
+              child: ListView.builder(
+                padding: const EdgeInsets.all(10),
+                itemCount: timeStamp.length,
+                itemBuilder: (context, index) {
+                  return Text(
+                      '${DateFormat.yMd().add_jm().format(timeStamp.elementAt(index))}');
+                },
+              ),
+            )
+          ],
         ),
+      ),
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            lastTime.timeStamp.add(DateTime.now());
+            lastTime.save();
+            Navigator.of(context).pop();
+          },
+          child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'Stamp Time',
+                textAlign: TextAlign.center,
+              )),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              textStyle: TextStyle(fontWeight: FontWeight.bold)),
+        )
       ],
     );
   }
-}
 
-Widget detailDialog(BuildContext context, LastTime lastTime) {
-  return AlertDialog(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-    title: Text(lastTime.job,
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-    content: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          RichText(
-            text: TextSpan(
-              style: DefaultTextStyle.of(context).style,
-              children: <TextSpan>[
-                TextSpan(
-                    text: 'Category : ',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                TextSpan(
-                    text: '${lastTime.category}',
-                    style: TextStyle(fontSize: 20)),
-              ],
-            ),
-          ),
-          SizedBox(width: 20),
-          Text(lastTime.timeStamp.length.toString()),
-        ],
-      ),
-    ),
-    actions: [
-      ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              'Stamp Time',
-              textAlign: TextAlign.center,
-            )),
-        style: ElevatedButton.styleFrom(
-            primary: Colors.green,
-            textStyle: TextStyle(fontWeight: FontWeight.bold)),
-      )
-    ],
-  );
-}
+  Widget deleteDialog(BuildContext context, LastTime lastTime) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      title: Text('Delete lasTime | Are you Sure?'),
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('No'),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.blueGrey,
+              textStyle: TextStyle(fontWeight: FontWeight.bold)),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            lastTime.delete();
+            Navigator.of(context).pop();
+          },
+          child: Text('Yes'),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              textStyle: TextStyle(fontWeight: FontWeight.bold)),
+        )
+      ],
+    );
+  }
 
-Widget deleteDialog(BuildContext context, LastTime lastTime) {
-  return AlertDialog(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-    title: Text('Delete lasTime | Are you Sure?'),
-    actions: [
-      ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        child: Text('No'),
-        style: ElevatedButton.styleFrom(
-            primary: Colors.blueGrey,
-            textStyle: TextStyle(fontWeight: FontWeight.bold)),
-      ),
-      ElevatedButton(
-        onPressed: () {
-          lastTime.delete();
-          Navigator.of(context).pop();
-        },
-        child: Text('Yes'),
-        style: ElevatedButton.styleFrom(
-            primary: Colors.red,
-            textStyle: TextStyle(fontWeight: FontWeight.bold)),
-      )
-    ],
-  );
-}
+  Future addLastTime(String job, String category) async {
+    final lastTime = LastTime()
+      ..job = job
+      ..category = category
+      ..createdDate = DateTime.now()
+      ..timeStamp.add(DateTime.now());
 
-Future addLastTime(String job, String category) async {
-  final lastTime = LastTime()
-    ..job = job
-    ..category = category
-    ..createdDate = DateTime.now()
-    ..timeStamp.add(DateTime.now());
-
-  final box = Hive.box<LastTime>('lastTimes');
-  box.put(job, lastTime);
-  print('$box, $lastTime');
+    final box = Hive.box<LastTime>('lastTimes');
+    box.put(job, lastTime);
+    print('$box, $lastTime');
+  }
 }
